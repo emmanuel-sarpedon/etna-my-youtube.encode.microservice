ec03482769fe0096162897da91ff2b64b0f28e89
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2022-08-02 08:53:19 +0400

    chore: refactor

--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,13 @@
-import axios, { AxiosResponse } from "axios";
-
 require("dotenv").config();
 import log4js from "log4js";
-import chokidar from "chokidar";
 const logger = log4js.getLogger("encode.service");
 logger.level = "trace";
 import * as service from "~/services/encode.services";
 
-import ffmpeg from "fluent-ffmpeg";
-if (process.env.FFMPEG_PATH) ffmpeg.setFfmpegPath(process.env.FFMPEG_PATH);
-if (process.env.FFPROBE_PATH) ffmpeg.setFfprobePath(process.env.FFPROBE_PATH);
-
 const videosPath = process.env.PUBLIC_PATH || "../public/videos/sources";
 
-chokidar
-  /* Watching the videosPath directory for ".mp4" new files that are added. */
-  .watch(videosPath + "/**/*.mp4", {
-    ignoreInitial: true,
-    alwaysStat: true,
-  })
-  .on("add", (path: string) => {
-    ffmpeg.ffprobe(path, async (err, metadata) => {
-      if (err) logger.error(err);
+service.startWatcher(videosPath);
 
-      if (metadata.format.format_name?.includes("mp4")) {
-        logger.info("New video uploaded :", path.split("/").slice(-1).join(""));
-
-        const fileName = path.split("/").slice(-1).join("");
-        const currentResolution = metadata.streams[0].height as number;
-        const missingResolutions = [1080, 720, 480, 360, 240, 144].filter(
-          (resolution) => resolution <= currentResolution
-        );
-        const [userId, videoId] = service.getIds(path, videosPath);
-
-        for (const resolution of missingResolutions) {
-          const outputFolder = [videosPath, userId, videoId, resolution].join(
-            "/"
-          );
-
-          await service.createVideoFolder(outputFolder);
-
-          const outputPath = [outputFolder, fileName].join("/");
-
-          /* Encoding the video and then patching the video with the new format and file. */
-          service
-            .encodeVideo(
-              path,
-              outputPath,
-              resolution as 1080 | 720 | 480 | 360 | 240 | 144
-            )
-            .then(() => {
-              const endpoint = process.env.API_URI + "/video/" + videoId;
-
-              axios
-                .patch(endpoint, {
-                  format: resolution,
-                  file: outputPath,
-                })
-                .then((res: AxiosResponse) => logger.info(res.data));
-            })
-            .catch((e) => logger.error(e));
-        }
-      }
-    });
-  });
+console.clear();
+logger.info("Encode service started");
+logger.info("Watching the videosPath directory for ".concat(videosPath));
--- src/services/encode.services.ts
+++ src/services/encode.services.ts
@@ -1,11 +1,71 @@
 import log4js from "log4js";
 import ffmpeg from "fluent-ffmpeg";
 import fs from "fs/promises";
+import chokidar from "chokidar";
+import axios from "axios";
 if (process.env.FFMPEG_PATH) ffmpeg.setFfmpegPath(process.env.FFMPEG_PATH);
 if (process.env.FFPROBE_PATH) ffmpeg.setFfprobePath(process.env.FFPROBE_PATH);
 
-const logger = log4js.getLogger("encode.service");
-logger.level = "trace";
+export function startWatcher(path: string) {
+  const logger = log4js.getLogger("encode.service");
+  logger.level = "trace";
+
+  chokidar
+    /* Watching the videosPath directory for ".mp4" new files that are added. */
+    .watch(path + "/**/*.mp4", {
+      ignoreInitial: true,
+      alwaysStat: true,
+    })
+    .on("add", (path: string) => {
+      ffmpeg.ffprobe(path, async (err, metadata) => {
+        if (err) logger.error(err);
+
+        if (metadata.format.format_name?.includes("mp4")) {
+          logger.info(
+            "New video uploaded :",
+            path.split("/").slice(-1).join("")
+          );
+
+          const fileName = path.split("/").slice(-1).join("");
+          const currentResolution = metadata.streams[0].height as number;
+          const missingResolutions = [1080, 720, 480, 360, 240, 144].filter(
+            (resolution) => resolution <= currentResolution
+          );
+          const [userId, videoId] = getIds(path, path);
+
+          for (const resolution of missingResolutions) {
+            const outputFolder = [path, userId, videoId, resolution].join("/");
+
+            await createVideoFolder(outputFolder);
+
+            const outputPath = [outputFolder, fileName].join("/");
+
+            /* Encoding the video and then patching the video with the new format and file. */
+            encodeVideo(
+              path,
+              outputPath,
+              resolution as 1080 | 720 | 480 | 360 | 240 | 144
+            )
+              .then(() => {
+                const endpoint = process.env.API_URI + "/video/" + videoId;
+
+                axios
+                  .patch(endpoint, {
+                    format: resolution,
+                    file: outputPath,
+                  })
+                  .then(() =>
+                    logger.info(
+                      `${outputPath.split("/").pop()} - ${resolution}p uploaded`
+                    )
+                  );
+              })
+              .catch((e) => logger.error(e));
+          }
+        }
+      });
+    });
+}
 
 export function getIds(
   videoPath: string,
@@ -19,12 +79,9 @@ export function getIds(
 }
 
 export async function createVideoFolder(videoFolder: string) {
-  await fs.mkdir(
-    videoFolder,
-    {
-      recursive: true,
-    },
-  );
+  await fs.mkdir(videoFolder, {
+    recursive: true,
+  });
 }
 
 export async function encodeVideo(
@@ -32,20 +89,29 @@ export async function encodeVideo(
   outputPath: string,
   resolution: 1080 | 720 | 480 | 360 | 240 | 144
 ) {
+  const logger = log4js.getLogger(outputPath.split("/").pop());
+  logger.level = "trace";
+
   try {
     return await new Promise<void>((resolve, reject): void => {
       ffmpeg(videoPath)
+        // * output path
         .output(outputPath)
+
+        // * resolution target
         .size(`?x${resolution}`)
+
+        // * event listeners
         .on("error", (err) => {
+          logger.error(err);
           reject(err);
         })
         .on("end", () => {
-          logger.info("Finished processing");
+          logger.mark(`${resolution}p encoding finished`);
           resolve();
         })
-        .on("progress", (progress) => {
-          logger.trace("Processing: " + Math.ceil(progress.percent) + "% done");
+        .on("start", () => {
+          logger.mark(`${resolution}p encoding started`);
         })
         .run();
     });
